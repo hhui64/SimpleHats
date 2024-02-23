2d42a0024da4970b2312d5d8bde235d12fded5e4
fonnymunkey <fonnymunkey@gmail.com>
2024-02-22 22:20:34 -0500

    0.2.3
    Fixed cauldron washing not working on servers

--- src/main/java/fonnymunkey/simplehats/common/EventHandler.java
+++ src/main/java/fonnymunkey/simplehats/common/EventHandler.java
@@ -9,6 +9,8 @@ import fonnymunkey.simplehats.common.item.HatItemDyeable;
 import fonnymunkey.simplehats.util.HatEntry;
 import fonnymunkey.simplehats.util.UUIDHandler;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.cauldron.CauldronInteraction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -36,6 +38,8 @@ public class EventHandler {
                     HatItem hat = entry.getHatDyeSettings().getUseDye() ? new HatItemDyeable(entry) : new HatItem(entry);
                     event.register(ForgeRegistries.Keys.ITEMS, new ResourceLocation(SimpleHats.modId, entry.getHatName()), () -> hat);
                     ModRegistry.hatList.add(hat);
+
+                    if(hat instanceof HatItemDyeable) CauldronInteraction.WATER.put((HatItemDyeable)hat, CauldronInteraction.DYED_ITEM);
                 }
                 SimpleHats.logger.log(Level.INFO, "Generated " + ModRegistry.hatList.size() + " hat items from hat entries.");
             }
